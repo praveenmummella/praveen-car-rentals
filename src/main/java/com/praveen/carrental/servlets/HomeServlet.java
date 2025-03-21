168f608bb89c39b96d992eaa9e4490522f7c4e1d
praveenmummella <80162116+praveenmummella@users.noreply.github.com>
2025-03-21 14:34:30 +0530

    Update HomeServlet.java

---
+++ src/main/java/com/praveen/carrental/servlets/HomeServlet.java
@@ -0,0 +1,15 @@
+package com.praveen.carrental.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+}
