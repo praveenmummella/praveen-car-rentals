436cf23b1fdbfe5008240a9a22b8fc1319b69c55
praveenmummella <80162116+praveenmummella@users.noreply.github.com>
2025-03-24 11:00:03 +0530

    Update HomeServlet.java

--- src/main/java/com/praveen/carrental/servlets/HomeServlet.java
+++ src/main/java/com/praveen/carrental/servlets/HomeServlet.java
@@ -1,15 +1,17 @@
 package com.praveen.carrental.servlets;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("index.jsp").forward(request, response);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Welcome to Praveen Car Rentals</h1>");
     }
 }
